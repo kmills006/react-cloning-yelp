fb33947584e19e3c05ce0c67ef25a2039d7a731a
Kristy Miller <miller.kristy06@gmail.com>
2016-05-28 20:35:14 -0500

    Install and import font-awesome to App component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'font-awesome/css/font-awesome.css';
+
 import styles from './styles.module.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className={styles.wrapper}>
-        <h1>Environment: {__NODE_ENV__}</h1>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: {__NODE_ENV__}
+        </h1>
       </div>
     );
   }
