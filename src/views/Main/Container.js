ca593ef51a398bcb025a65716f953ff11dfe3b1a
Kristy Miller <miller.kristy06@gmail.com>
2016-06-03 15:36:15 -0500

    Remove rouge console.log

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export class Container extends React.Component {
   render() {
-    console.log('hello from container');
     return (
       <div>Hello from the container</div>
     );
