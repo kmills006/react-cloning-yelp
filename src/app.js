e4253b52ddc53469c8d18d090b673d422f015f4f
Kristy Miller <miller.kristy06@gmail.com>
2016-06-03 13:31:48 -0500

    Import ReactRouter to App compoment and set up initial route location

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import 'font-awesome/css/font-awesome.css';
 import './app.css';
 
 import App from 'containers/App/App';
+// Routers
+import { browserHistory, Route, Router } from 'react-router';
+const routes = (
+  <Router>
+    <Route path="/" component={Home} />
+  </Router>
+);
 
 const mountNode = document.querySelector('#root');
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <App history={browserHistory} routes={routes} />, mountNode);
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
 
 import styles from './styles.module.css';
 
 class App extends React.Component {
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    routes: PropTypes.object.isRequired,
+  }
+
+  get content() {
+    return (
+      <Router history={this.props.history} routes={this.props.routes} />
+    );
+  }
+
   render() {
     return (
-      <div className={styles.wrapper}>
-        <h1>
-          <i className="fa fa-star"></i>
-          Environment: {__NODE_ENV__}
-        </h1>
+      <div style={{ height: '100%' }}>
+        {this.content}
       </div>
-    );
+    )
   }
 }
 
