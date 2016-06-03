10a50756ef7278b5eb8d3ce866b12302dedee448
Kristy Miller <miller.kristy06@gmail.com>
2016-06-03 13:32:16 -0500

    Home page empty component

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import 'font-awesome/css/font-awesome.css';
 import './app.css';
 
 import App from 'containers/App/App';
+import Home from 'components/Home';
+
 // Routers
 import { browserHistory, Route, Router } from 'react-router';
 const routes = (
